d98fba093289ea89322380aa623c177f9372d912
Nicholas <nicholasss@users.noreply.github.com>
2025-04-15 11:52:26 -0400

    first try of implementation
    asdf

---
+++ merge_sort.c
@@ -0,0 +1,15 @@
+#include "merge_sort.h"
+#include <stdio.h>
+
+void merge_sort(int arr[], size_t length) {
+  // divide the list into halves until it cannot be divided anymore
+  // finding the midpoint of array
+  int mid = (int)length / 2;
+  printf("midpoint of %d length is %d\n", (int)length, mid);
+
+  // conquer the subdivisions by sorting them using the algorithm
+  //
+  // merge the sorted subarrays until they are back in order
+
+  return;
+}
---
+++ merge_sort.h
@@ -0,0 +1,8 @@
+#ifndef MERGE_SORT
+#define MERGE_SORT
+
+#include <stdlib.h>
+
+void merge_sort(int arr[], size_t length);
+
+#endif // MERGE_SORT

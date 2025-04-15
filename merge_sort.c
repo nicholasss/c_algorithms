41176117b002de1f8e7dcfcb7178422ddc67569d
Nicholas <nicholasss@users.noreply.github.com>
2025-04-15 12:47:28 -0400

    finished helper function

--- merge_sort.c
+++ merge_sort.c
@@ -1,10 +1,51 @@
 #include "merge_sort.h"
 #include <stdio.h>
 
-// merges the two subarrays
+// merges the two subarrays, actually does sorting on merge
 // first is:  arr[left...mid]
 // second is: arr[mid + 1...right]
-void merge(int arr[], int left, int mid, int right) { return; }
+void merge(int arr[], int left, int mid, int right) {
+  int arrALength = mid - left + 1;
+  int arrBLength = right - mid;
+
+  // create temp arrays and copy data to them
+  int arrA[arrALength], arrB[arrBLength];
+  for (int i = 0; i < arrALength; i++) {
+    arrA[i] = arr[left + i];
+  }
+  for (int i = 0; i < arrBLength; i++) {
+    arrB[i] = arr[mid + 1 + i];
+  }
+
+  // merge temp arrays back into arr[left...right]
+  int x = 0;
+  int y = 0;
+  int z = left;
+  while (x < arrALength && y < arrBLength) {
+    if (arrA[x] <= arrB[y]) {
+      arr[z] = arrA[x];
+      x++;
+    } else {
+      arr[z] = arrB[y];
+      y++;
+    }
+    z++;
+  }
+
+  // copy remaining elements of arrA
+  while (x < arrALength) {
+    arr[z] = arrA[x];
+    x++;
+    z++;
+  }
+
+  // copy remaining elements of arrB
+  while (y < arrBLength) {
+    arr[z] = arrB[y];
+    y++;
+    z++;
+  }
+}
 
 // this is the function that is called
 // left is the first index of array
--- merge_sort.h
+++ merge_sort.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+void merge(int arr[], int left, int mid, int right);
+
 void merge_sort(int arr[], int left, int right);
 
 #endif // MERGE_SORT

f69e4187f63d53c941be6d8f5254b5330485eb4d
Nicholas <nicholasss@users.noreply.github.com>
2025-04-15 12:25:01 -0400

    organizing and commenting on entry function

--- merge_sort.c
+++ merge_sort.c
@@ -10,21 +10,15 @@ void merge(int arr[], int left, int mid, int right) { return; }
 // left is the first index of array
 // right is the last index of array
 void merge_sort(int arr[], int left, int right) {
-  // divide the list into halves until it cannot be divided anymore
-  // finding the midpoint of array
-
+  // if statement recurses until it reaches a sorted array (one element)
   if (left < right) {
     int mid = left + (right - 1) / 2;
 
-    // sort the first and second halves
+    // recursive calls to sort the two halves
     merge_sort(arr, left, mid);
     merge_sort(arr, mid + 1, right);
+
+    // merge the two halves to together
     merge(arr, left, mid, right);
   }
-
-  // conquer the subdivisions by sorting them using the algorithm
-  //
-  // merge the sorted subarrays until they are back in order
-
-  return;
 }
